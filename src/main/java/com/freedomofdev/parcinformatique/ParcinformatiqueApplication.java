a78631033d77c9ca89fa40246521be84f61a74ec
Fares Masmoudi <faresmasmoudi@outlook.com>
2024-03-14 12:55:46 +0100

    created spring boot project

---
+++ src/main/java/com/freedomofdev/parcinformatique/ParcinformatiqueApplication.java
@@ -0,0 +1,13 @@
+package com.freedomofdev.parcinformatique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcinformatiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcinformatiqueApplication.class, args);
+	}
+
+}
