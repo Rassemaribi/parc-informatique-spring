9e55ab461cb05da4bcd7d7ffa3ced39acb31c09c
Fares Masmoudi <faresmasmoudi@outlook.com>
2024-03-20 19:16:50 +0100

    renamed enitiy demandeAcquisition

--- src/main/java/com/freedomofdev/parcinformatique/entity/DemandeAcquisition.java
+++ src/main/java/com/freedomofdev/parcinformatique/entity/DemandeAcquisition.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "demande_acquisitions")
+@Entity(name = "demandes_acquisition")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
