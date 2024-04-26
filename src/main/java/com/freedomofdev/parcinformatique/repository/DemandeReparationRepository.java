35cde159212f7a1759b33af3a14d007deefd9718
Fares Masmoudi <faresmasmoudi@outlook.com>
2024-04-26 10:05:17 +0100

    add reparationHandledBy

--- src/main/java/com/freedomofdev/parcinformatique/repository/DemandeReparationRepository.java
+++ src/main/java/com/freedomofdev/parcinformatique/repository/DemandeReparationRepository.java
@@ -2,6 +2,7 @@ package com.freedomofdev.parcinformatique.repository;
 
 import com.freedomofdev.parcinformatique.entity.Actif;
 import com.freedomofdev.parcinformatique.entity.DemandeReparation;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Repository
 public interface DemandeReparationRepository extends JpaRepository<DemandeReparation, Long> {
+    @EntityGraph(attributePaths = {"reparationHandledBy"})
     List<DemandeReparation> findByReparationRequestedBy_Id(Long userId);
 
     List<DemandeReparation> findByActifAndActive(Actif actif, boolean active);
