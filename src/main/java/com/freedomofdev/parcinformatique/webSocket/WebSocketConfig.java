71fa49055d512ee1527544519951ba58e30a1700
Rassemaribi <Racem.Aribi@iit.ens.tn>
2024-07-17 14:58:24 +0100

    update

--- src/main/java/com/freedomofdev/parcinformatique/webSocket/WebSocketConfig.java
+++ src/main/java/com/freedomofdev/parcinformatique/webSocket/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat-websocket").setAllowedOrigins("http://localhost:4200").withSockJS();
+        registry.addEndpoint("/chat-websocket").setAllowedOrigins("https://parcinformatiquefodservicess.azurewebsites.net/").withSockJS();
     }
 
 
