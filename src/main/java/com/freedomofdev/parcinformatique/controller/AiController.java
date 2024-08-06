540892d1b7a8b38e945a9ffc0225fd7e01dca6fc
Rassemaribi <Racem.Aribi@iit.ens.tn>
2024-08-06 11:10:52 +0100

    Update AiController.java

--- src/main/java/com/freedomofdev/parcinformatique/controller/AiController.java
+++ src/main/java/com/freedomofdev/parcinformatique/controller/AiController.java
@@ -7,12 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
-
-@CrossOrigin(origins = "https://parcinformatiquefodservicess.azurewebsites.net", maxAge = 3600, allowCredentials = "true") // Adjust the origins to match your Angular app's URL
+@CrossOrigin(origins = "https://parcinformatiquefodservicess.azurewebsites.net", maxAge = 3600, allowCredentials = "true")// Adjust the origins to match your Angular app's URL
 @RequestMapping("/api")
 public class AiController {
 
@@ -20,26 +16,14 @@ public class AiController {
     private AiModelService aiModelService;
 
     @PostMapping("/recommend")
-    public ResponseEntity<String> getRecommendations(@RequestBody InputPayload payload) {
-        aiModelService.getRecommendations(payload);
-        return ResponseEntity.ok("Recommendations processed");
+    public ResponseEntity<Object> getRecommendations(@RequestBody InputPayload payload) {
+        Object recommendations = aiModelService.getRecommendations(payload);
+        return ResponseEntity.ok(recommendations);
     }
 
     @PostMapping("/inputCriteria")
     public ResponseEntity<InputPayload> createInputCriteria(@RequestBody InputPayload inputPayload) {
         InputPayload savedInputPayload = aiModelService.saveInputCriteria(inputPayload);
         return new ResponseEntity<>(savedInputPayload, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/recommendAndInputCriteria/{id}")
-    public ResponseEntity<Map<String, Object>> getRecommendAndInputCriteria(@PathVariable Long id) {
-        InputPayload inputPayload = aiModelService.getInputCriteria(id);
-        String recommendations = aiModelService.getRecommendations(inputPayload).toString();
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("recommendations", recommendations);
-        response.put("inputCriteria", inputPayload);
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
