95b30aab203ab08a4f41bc5bccaa5a4012b514c4
Rassemaribi <Racem.Aribi@iit.ens.tn>
2024-08-06 16:33:32 +0100

    update

--- src/main/java/com/freedomofdev/parcinformatique/entity/Laptop.java
+++ src/main/java/com/freedomofdev/parcinformatique/entity/Laptop.java
@@ -2,10 +2,10 @@ package com.freedomofdev.parcinformatique.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "BestLaptops")
+
+
+@Entity(name = "BestLaptops")
 public class Laptop {
 
     @Id
